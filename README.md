6502be7832cb14a3ead74ded51360fea8da17f99
Wagner Paulino <wagnersantos9778@gmail.com>
2020-09-27 16:07:43 -0300

    laboratório de autenticação

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 Steps to run this project:
 
 1. Run `npm i` command
-2. Run `npm start` command
+2. Run `typeorm migration:create -n CreateAdminUser` command
+3. Run `npm start` command
+4. Insert the first admin user `npm run migration:run` command
---
+++ src/migration/1601233164521-CreateAdminUser.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAdminUser1601233164521 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
